8bd7c40ec063e8688fe31042e81488fb5a0bafdf
Xs <64141791+lllllllllllll-llll@users.noreply.github.com>
2020-09-11 17:06:27 +0800

    Update calculate.m

--- calculate.m
+++ calculate.m
@@ -1,7 +1,7 @@
-sq_data = load('E:\00CODE00\0main\IQA methods\MEON\csiq\full\cc\sq.txt');
+sq_data = load('your predicted scores txt path. e.g. D:/cal/predicted.txt');
 sq = sq_data(:, 1);
 
-q_data = load('E:\00CODE00\0main\IQA methods\MEON\csiq\full\cc\q.txt');
+q_data = load('your ground truth scores txt path. e.g. D:/cal/gt.txt');
 q = q_data(:, 1);
 
 [SROCC,PLCC,ypre] = nonlinearfitting(q, sq);
